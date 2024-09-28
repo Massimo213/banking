3df7e06158065162d3018e0a02fe1abe5581bb38
Massimo213 <yahyastartwell@gmail.com>
2024-09-27 23:36:04 -0400

    Implement plaid link,Dwallo

--- src/components/AuthForm.tsx
+++ src/components/AuthForm.tsx
@@ -21,6 +21,7 @@ import { authFormSchema } from "../../lib/utils";
 import { Loader2 } from "lucide-react";
 import { getLoggedInUser, signIn, signUp } from "../../lib/actions/user.actions";
 import { useRouter } from "next/navigation";
+import PlaidLink from "./PlaidLink";
 
 
 const formSchema = z.object({
@@ -51,7 +52,19 @@ const AuthForm = ({ type }: { type: string }) => {
       console.log("Form Data:", data); // Check if data is correct
   
       if (type === "sign-up") {
-        const newUser = await signUp(data);
+        const userData = {
+          firstName: data.firstName!,
+          lastName: data.lastName!,
+          address1: data.address1!,
+          city: data.city!,
+          state: data.state!,
+          postalCode: data.postalCode!,
+          dateOfBirth: data.dateOfBirth!,
+          ssn: data.ssn!,
+          email: data.email,
+          password: data.password
+        }
+        const newUser = await signUp(userData);
 
         setUser(newUser);
         
@@ -104,9 +117,11 @@ const AuthForm = ({ type }: { type: string }) => {
           </h1>
         </div>
       </header>
-      {user ? (
-        <div className="flex flex-col gap-4">{/*PlaidLink */}</div>
-      ) : (
+       {user ? (
+        <div className="flex flex-col gap-4">
+        <PlaidLink user={user} variant='primary' />
+          </div>
+       ) : ( 
         <>
           <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
@@ -159,7 +174,7 @@ const AuthForm = ({ type }: { type: string }) => {
             
           </footer>
         </>
-      )}
+       )} 
     </section>
   );
                   }
